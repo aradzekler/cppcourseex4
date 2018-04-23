530b7ea5b4c0861bdd2ef3ccf04fd3f42d764687
aradzekler <25280958+aradzekler@users.noreply.github.com>
2018-04-23 17:08:21 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -13,21 +13,19 @@ class CircularInt
     public:
     int current;
 
-    CircularInt(int start_input,int end_input);
-    CircularInt& operator++();
-    CircularInt& operator++(int);
-    CircularInt& operator--(int);
-    CircularInt& operator+=(int value); 
-    CircularInt& operator-=(int value);
+    CircularInt(int smallest,int largest); //ready
+    CircularInt& operator++(int); //ready
+    CircularInt& operator--(int); //ready
+    CircularInt& operator+=(int value); //ready
+    CircularInt& operator-=(int value); //ready
     CircularInt& operator*=(int value); 
-     CircularInt& operator/=(int value);
-    const CircularInt& operator= (int value);
-    bool operator==(const CircularInt other)const;
-    bool operator!=(const CircularInt other)const;
+    CircularInt& operator/=(int value);
+    const CircularInt& operator=(int value);
+    bool operator==(const CircularInt other)const; 
+    bool operator!=(const CircularInt other)const; 
     int& operator*();
     operator bool() ;
-        
-        ~CircularInt();
+    ~CircularInt();
 
 };
  ostream &operator<<(ostream& os, const CircularInt& dt);
